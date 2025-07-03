40155f7d98ac985f15326dd75e32e9b4935b8511
Dhiraj Patra <dhiraj.patra@gmail.com>
2025-07-03 11:19:16 +0530

    updated

--- azure_main.py
+++ azure_main.py
@@ -66,9 +66,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(datetime.timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(datetime.timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
